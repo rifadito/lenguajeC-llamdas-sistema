663d610bb9a8f22b0f3b536c9a9cf48ccbf92968
root <root@localhost.localdomain>
2017-01-31 20:52:31 -0600

    Practica6

--- practica6/shrink_me.c
+++ practica6/shrink_me.c
@@ -4,9 +4,9 @@
 #include<limits.h>
 int main()
 {
-	unsigned int var1,var2,mayor;
+	unsigned int var1,var2;
 	printf("Ingresa dos enteros sin signo(maximo %u):",UINT_MAX);
 	scanf("%d",&var1);
 	scanf("%d",&var2);
-	printf((var1>var2)?"El mayor es %d\n":"El menor es %d",var1);
+	printf((var1>var2)?"El mayor es %d\n":"El menor es %d",var1,var2);
 }
